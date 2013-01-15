aa6fd0e570bc8f6472a73cdd4b3902487e2b667f
sourcebits-harshitakasera <harshita.kasera@sourcebits.com>
2013-01-15 16:58:25 +0530

    Initial Commit

---
+++ js/mep-feature-captionator.js
@@ -0,0 +1,33 @@
+(function ($) {
+    // captionator
+
+    $.extend(mejs.MepDefaults, {
+       captionFont:"",
+       captionSize:"",
+       captionColor:"",
+       captionBackground:"",
+       captionPosition:"" 
+    });
+
+    $.extend(MediaElementPlayer.prototype, {
+        buildcaptionator: function (player, controls, layers, media) {
+            var $caption_text_container = $('.mejs-captions-text'),
+                $caption_container = $caption_text_container.parent();
+
+            if(player.options.captionPosition == "top"){
+                    $caption_container.parent().css("top","0px");
+                    $caption_container.css("bottom","1px");
+                }
+
+            if(player.options.captionBackground){
+                $caption_container.css("background",player.options.captionBackground);
+                $caption_text_container.css("background","none");
+            }
+            $caption_text_container.css("font-family",player.options.captionFont);
+            $caption_text_container.css("font-size",player.options.captionSize);
+            $caption_text_container.css("color",player.options.captionColor);
+
+        }
+    });
+
+})(mejs.$);
